27637a13ce374b055412c03b6d5d3f6e85259dfd
Administrator <Administrator@samsung-fec2a15>
2011-01-31 16:58:26 +0900

    1st commit

---
+++ src/Git.java
@@ -0,0 +1,12 @@
+
+public class Git {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("Hello GIT ^^");
+	}
+
+}
